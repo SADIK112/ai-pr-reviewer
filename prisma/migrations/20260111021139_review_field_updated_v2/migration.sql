a506f27f40d48690ba10a63986ab49002444671a
Sadikur Rahman <sadikur.rahman@bevycommerce.com>
2026-01-11 22:47:43 -0500

    Ignore generated / local files

--- prisma/migrations/20260111021139_review_field_updated_v2/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `wasAccepted` on the `Suggestion` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Suggestion" DROP COLUMN "wasAccepted";
