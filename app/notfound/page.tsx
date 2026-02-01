4e911d33d9984e18a6e4015bf4994a9f460efc42
Sadikur Rahman <sadikur.rahman@bevycommerce.com>
2026-02-01 01:15:22 -0500

    removed unsued import

--- app/notfound/page.tsx
+++ app/notfound/page.tsx
@@ -1,10 +1,4 @@
-import { useEffect } from "react";
-
 const NotFound = () => {
-
-  // useEffect(() => {
-  //   console.error("404 Error: User attempted to access non-existent route:", location.pathname);
-  // }, [location.pathname]);
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-muted">
